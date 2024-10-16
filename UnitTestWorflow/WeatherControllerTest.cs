e95a65b9b6e0f672bba0ae71230da837c1ff3030
SudhanshuKumar9 <sud4499@gmail.com>
2024-10-16 09:33:26 +0530

    add test appliation

---
+++ UnitTestWorflow/WeatherControllerTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.Logging;
+using Moq;
+using WorflowTestApp.Controllers;
+
+namespace UnitTestWorflow
+{
+    public class WeatherControllerTest
+    {
+        private readonly WeatherForecastController weatherForecastController;
+        private readonly Mock<ILogger<WeatherForecastController>> _mockLoger;
+        public WeatherControllerTest()
+        {
+            _mockLoger = new Mock<ILogger<WeatherForecastController>>();
+            weatherForecastController = new WeatherForecastController(_mockLoger.Object);
+        }
+
+        [Fact]
+        public void TestGetWeatherForecastResult()
+        {
+            var result = weatherForecastController.Get();
+            Assert.NotNull(result);
+        }
+    }
+}
